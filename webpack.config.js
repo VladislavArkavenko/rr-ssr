2fe68895b1b59426acebe7634d2004e0ec0af970
Vladislav Arkavenko <suzdal.den0097@yandex.ua>
2018-10-22 18:59:28 +0300

    initial comit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+let path = require('path')
+let webpack = require('webpack')
+let nodeExternals = require('webpack-node-externals')
+
+let browserConfig = {
+    entry: './src/browser/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader'
+            },
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __isBrowser__: "true"
+        })
+    ]
+}
+
+let serverConfig = {
+    entry: './src/server/index.js',
+    target: 'node',
+    externals: [nodeExternals()],
+    output: {
+        path: __dirname,
+        filename: 'server.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __isBrowser__: "false"
+        })
+    ]
+}
+
+module.exports = [browserConfig, serverConfig]
